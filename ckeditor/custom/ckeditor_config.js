d06147af6a50702728c24e85ce52f9e380643b50
Skorney <skorney@list.ru>
2012-01-17 14:58:29 +0200

    added image icon to popupimage and define toolbars

--- ckeditor/custom/ckeditor_config.js
+++ ckeditor/custom/ckeditor_config.js
@@ -3,29 +3,34 @@ CKEDITOR.editorConfig = function( config )
     //подключаем пользовательские plugins
     config.extraPlugins = 'popupimage';
 
-    config.toolbar = 'Custom';
-    config.toolbar_Custom =
+    config.toolbar = 'Content';
+    config.toolbar_Content =
         [
             { name: 'document', items : [ 'Source', 'NewPage','Preview' ] },
             { name: 'clipboard', items : [ 'Cut','Copy','Paste','PasteText','PasteFromWord','-','Undo','Redo' ] },
             { name: 'editing', items : [ 'Find','Replace','-','SelectAll','-','Scayt' ] },
-            { name: 'insert', items : [ 'Image','Flash','Table','HorizontalRule','Smiley','SpecialChar','PageBreak'
-                ,'Iframe', 'PopupImage' ] },
-            '/',
-            { name: 'styles', items : [ 'Styles','Format' ] },
+            { name: 'insert', items : [ 'PopupImage','Table','SpecialChar' ] },
             { name: 'basicstyles', items : [ 'Bold','Italic','Strike','-','RemoveFormat' ] },
             { name: 'paragraph', items : [ 'NumberedList','BulletedList','-','Outdent','Indent','-','Blockquote' ] },
             { name: 'links', items : [ 'Link','Unlink','Anchor' ] },
-            { name: 'tools', items : [ 'Maximize','-','About' ] }
+            { name: 'tools', items : [ 'Maximize' ] }
+        ];
+
+    config.toolbar = 'Inline';
+    config.toolbar_Inline =
+        [
+            { name: 'clipboard', items : [ 'PasteText','PasteFromWord' ] },
+            { name: 'basicstyles', items : [ 'Bold','Italic','Strike','-','RemoveFormat' ] },
+            { name: 'links', items : [ 'Link','Unlink' ] }
         ];
 
     /* пути к скриптам для выбра*/
-    config.filebrowserBrowseUrl = './kcfinder/browse.php?type=files&dir=images/1';
+    config.filebrowserBrowseUrl = './kcfinder/browse.php?type=files';
     config.filebrowserImageBrowseUrl = './kcfinder/browse.php?type=images&dir=images/1';
     config.filebrowserFlashBrowseUrl = './kcfinder/browse.php?type=flash';
 
     /* пути к скриптам-загрузчикам */
-    config.filebrowserUploadUrl = './kcfinder/upload.php?type=files&dir=images/1';
+    config.filebrowserUploadUrl = './kcfinder/upload.php?type=files';
     config.filebrowserImageUploadUrl = './kcfinder/upload.php?type=images&dir=images/1';
     config.filebrowserFlashUploadUrl = './kcfinder/upload.php?type=flash';
 
